f5b795c5710f0979fd447b1b26374cb72b5c75a3
amitbendale8 <bendale.amit7@gmail.com>
2015-06-10 08:16:21 +0000

    change

---
+++ config/initializers/devise.rb
@@ -0,0 +1,5 @@
+# Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
+Devise.setup do |config|
+  Devise.secret_key = "b78db4d01372c41a13aa815461f4757ff7cfd4256094523673a3d3ab7c7dfc15c8654a744ef948108ff5029d394be3340f513f63040a3602664a5df116c7cdfb"
+end
